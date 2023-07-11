a2cbaea1823fa9cb75e1ba904db71aab80456be4
clintonjohn-cmd <johnouko@kabarak.ac.ke>
2023-07-12 00:50:49 +0300

    dark/light mode commit

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function login() {
+//The login/ sign up section
+let getSubmit = document.getElementById("submit-button");
+getSubmit.onclick = function(event) {
+        event.preventDefault();
+        //   window.open("index.html");
+        window.location.href = "index.html";
+        //window.history.back();
+    }
+    /*
+    // The Dark/Light Mode
+    let getBody = document.body;
+    let getDark = document.getElementById("dark");
+    getDark.onclick = function() {
+        getBody.style.cssText = "background-color:#111;";
+        document.body.classList.toggle("light-theme");
+    }
+    */
+function darkTheme() {
+    var getMode = document.getElementById("mode-select").value;
+    if (getMode == "Lightmode") {
 
+        document.body.classList.toggle("light-theme");
+    } else {
+        document.body.classList.toggle("dark-theme");
+    }
 }
